0610765d6bfa4bbf60978ee64bbfe0edd1e61ec2
Jorge8790 <123428451+Jorge8790@users.noreply.github.com>
2023-03-01 11:03:44 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+
+//funcion ocultar o mostrar menu
+function mostrarOcultarMenu(){
+    if (menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //ocultar menu 
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+
+//aplica animaciones de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("comunicacion");
+        habilidades[4].classList.add("trabajo");
+        habilidades[5].classList.add("creatividad");
+        habilidades[6].classList.add("dedicacion");
+    }
+}
+
+
+//scroll para aplicar la animacion en la barra
+window.onscroll = function(){
+    efectoHabilidades();
+}
